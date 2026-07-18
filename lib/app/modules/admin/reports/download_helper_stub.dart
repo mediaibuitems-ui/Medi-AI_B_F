8b51d2cff351ffa37c47b3477574fbce0cf714ad
Medi-AI_F-B <abdul.man7080@gmail.com>
2026-07-19 02:38:00 +0500

    Fix missing flutter material import

--- lib/app/modules/admin/reports/download_helper_stub.dart
+++ lib/app/modules/admin/reports/download_helper_stub.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 Future<void> downloadFile(String content, String filename) async {
   try {
